f7f95b737278210f887403b29c5c2a50582ad91c
Nik Nair <nikhiln822@gmail.com>
2018-10-29 12:54:11 -0400

    Code Update For MavLink
    Now can successfully import values

--- Experimental/THE WHOLE PAK/mavlinkClient.py
+++ Experimental/THE WHOLE PAK/mavlinkClient.py
@@ -1,1 +1,15 @@
-mavutil.mavlink_connection('tcp:localhost:8080', planner_format=False, notimestamps=True, robust_parsing=True) 
+from __future__ import print_function
+
+import time
+
+from pymavlink import mavutil
+
+
+newdevice = mavutil.mavlink_connection('tcp:localhost:8080', planner_format=False, notimestamps=True, robust_parsing=True) 
+
+lolWhile = 9
+
+while (lolWhile >0):
+	  data = newdevice.recv()
+	  print(data)
+	  time.sleep(0.001)
