bb009f6083ee24b2f65a530e42e3b301036d5907
Nik Nair <nikhiln822@gmail.com>
2018-10-29 13:23:56 -0400

    New Socket
    The socket enables mavlink to recive updated data

---
+++ Experimental/THE WHOLE PAK/socpy/STFU.py
@@ -0,0 +1,18 @@
+import socket 
+import time
+import random
+
+
+port = 8080 
+s.bind(('', port))   
+print "socket binded to %s" %(port)
+s.listen(5)     
+print "socket is listening" 
+
+c, addr = s.accept()  
+print 'Got connection from', addr
+
+while True:
+	randInt = random.randint(1,101)
+	s.send(randInt)
+	
