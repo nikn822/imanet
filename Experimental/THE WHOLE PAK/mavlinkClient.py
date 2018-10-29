8c13174e68bf87b4e37c300c915074e9b8314010
Nik Nair <nikhiln822@gmail.com>
2018-10-29 18:21:14 -0400

    New Window Alg
    alg for window is kinda started development

--- Experimental/THE WHOLE PAK/mavlinkClient.py
+++ Experimental/THE WHOLE PAK/mavlinkClient.py
@@ -5,21 +5,19 @@ import time
 from pymavlink import mavutil
 from tkinter import *
 
-def makeWin():
-	root = Tk()
-	lab = Label(root, text="this is too easy")
-	root.mainloop()
-
-
-#makeNewWindow = makeWin
+root = Tk()
+lab = Label(root, text="this is too easy")
+lab.pack()
+root.mainloop()
 
 
 
-newdevice = mavutil.mavlink_connection('tcp:10.0.2.15:7850', planner_format=False, notimestamps=True, robust_parsing=True) 
+newdevice = mavutil.mavlink_connection('tcp:10.105.39.111:7850', planner_format=False, notimestamps=True, robust_parsing=True) 
 
 lolWhile = 9
 
 while (lolWhile >0):
 	  data = newdevice.recv()
 	  print(data)
+
 
