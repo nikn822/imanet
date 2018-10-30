2a421d1ceab5618fe30b2c7bc386e423f5052a9b
Nik Nair <nikhiln822@gmail.com>
2018-10-29 23:40:32 -0400

    GUI issues
    Issues at gui have gotten it pulled for now... lets see if we can change this later

--- Experimental/THE WHOLE PAK/mavlinkClient.py
+++ Experimental/THE WHOLE PAK/mavlinkClient.py
@@ -3,29 +3,8 @@ from __future__ import print_function
 import time
 
 from pymavlink import mavutil
-from tkinter import *
 
-master = Tk()
-jeff = Label(master, text="First Name").grid(row=0)
-riperino = Label(master, text="Last Name").grid(row=1)
-
-e1 = Entry(master, textvariable=v)
-e2 = Entry(master, textvariable=q)
-
-s = v.get()
-p = q.get()
-
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-
-mainloop( )
-
-print(jeff)
-print(riperino)
-
-
-
-newdevice = mavutil.mavlink_connection('tcp:localhost:7850', planner_format=False, notimestamps=True, robust_parsing=True) 
+newdevice = mavutil.mavlink_connection('tcp:localhost:7850', planner_format=True, notimestamps=False, robust_parsing=True) 
 
 lolWhile = 9
 
