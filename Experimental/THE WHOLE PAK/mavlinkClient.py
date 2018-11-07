d69327d9ccec6c43a697e6a8c5e6e41b445dae94
Nik Nair <nikhiln822@gmail.com>
2018-11-06 23:56:33 -0500

    Live Plotting
    This finishes most of the features of the code...now to squash some bugs

--- Experimental/THE WHOLE PAK/mavlinkClient.py
+++ Experimental/THE WHOLE PAK/mavlinkClient.py
@@ -6,6 +6,21 @@ from pymavlink import mavutil
 
 from tkinter import *
 
+from bokeh.io import curdoc
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import Figure
+
+import csv
+import pandas as pd
+
+import numpy as np
+import socket 
+import ast 
+
+data = 0
+ct = 0
+lolWhile = 9
+
 
 def GuiStartCMD():
 
@@ -38,11 +53,28 @@ def GuiStartCMD():
 startGui = GuiStartCMD()
 newdevice = mavutil.mavlink_connection(('tcp:'+startGui), planner_format=True, notimestamps=False, robust_parsing=True) 
 
-lolWhile = 9
+source = ColumnDataSource(dict(s=[], r=[]))
+r = Figure(width=1000, height=300, title='Random Number Recieved')
+r.line(source=source, x='s', y='r', line_width=2, alpha= .85, color= 'purple')
 
-while (lolWhile >0):
-	  time.sleep(0.0100)
-	  data = str (newdevice.recv())
-	  print(data)
+def update_data():
+		global ct, data
+	    ct += 1
+
+
+	while (lolWhile >0):
+		time.sleep(0.0100)
+		data = int (newdevice.recv())
+		print(data)
+
+		
+	new_data = dict(s=[ct],y=[data])
+    source.stream(new_data,100)
+
+
+
+
+curdoc().add_root(r)
+curdoc().add_periodic_callback(update_data, 100)
 
 
